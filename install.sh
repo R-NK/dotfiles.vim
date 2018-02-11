8613860eed8016601795385767e06c7003e3f2c4
R-NK <right.rcon.f@outlook.com>
2018-02-12 06:56:33 +0900

    install.shを修正

--- install.sh
+++ install.sh
@@ -6,12 +6,13 @@ git submodule update
 unameOut="$(uname -s)"
 case $unameOut in
 	Linux )	
-			target_vim="~/dotfiles.vim/.vim"
-			link_vim="~/.vim"
-			target_vimrc="~/dotfiles.vim/.vimrc"
-			link_vimrc="~/.vimrc"
-			ln -s -f $target_vim $link_vim
-			ln -s -f $target_vimrc $link_vimrc ;;
+			user=~
+			target_vim="/dotfiles.vim/.vim"
+			link_vim="/.vim"
+			target_vimrc="/dotfiles.vim/.vimrc"
+			link_vimrc="/.vimrc"
+			ln -s -f $user$target_vim $user$link_vim
+			ln -s -f $user$target_vimrc $user$link_vimrc ;;
 	MINGW* )
 			target_vim="\dotfiles.vim\.vim"
 			link_vim="\.vim"
