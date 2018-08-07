505ce0a9ca337cd4e44302e4487f143b775fe8f3
Ryo Nakatani <right@RyonoMacBook-Pro.local>
2018-08-08 04:09:46 +0900

    add OSX setting

--- install.sh
+++ install.sh
@@ -5,19 +5,27 @@ git submodule update
 
 unameOut="$(uname -s)"
 case $unameOut in
+	Darwin )
+		target_vim="/.vim"
+		target_vimrc="/.vimrc"
+		link_vim="/.vim"
+		link_vimrc="/.vimrc"
+		ln -s -f $PWD$target_vim $HOME$link_vim
+		ln -s -f $PWD$target_vimrc $HOME$link_vimrc ;;
+
 	Linux )	
-			user=~
-			target_vim="/dotfiles.vim/.vim"
-			link_vim="/.vim"
-			target_vimrc="/dotfiles.vim/.vimrc"
-			link_vimrc="/.vimrc"
-			ln -s -f $user$target_vim $user$link_vim
-			ln -s -f $user$target_vimrc $user$link_vimrc ;;
+		target_vim="/.vim"
+		target_vimrc="/.vimrc"
+		link_vim="/.vim"
+		link_vimrc="/.vimrc"
+		ln -s -f $PWD$target_vim $HOME$link_vim
+		ln -s -f $PWD$target_vimrc $HOME$link_vimrc ;;
+
 	MINGW* )
-			target_vim="\dotfiles.vim\.vim"
-			link_vim="\.vim"
-			target_vimrc="\dotfiles.vim\.vimrc"
-			link_vimrc="\.vimrc"
-			cmd //c "mklink /D %USERPROFILE%${link_vim} %USERPROFILE%${target_vim}"
-			cmd //c "mklink %USERPROFILE%${link_vimrc} %USERPROFILE%${target_vimrc}" ;;
+		target_vim="\dotfiles.vim\.vim"
+		link_vim="\.vim"
+		target_vimrc="\dotfiles.vim\.vimrc"
+		link_vimrc="\.vimrc"
+		cmd //c "mklink /D %USERPROFILE%${link_vim} %USERPROFILE%${target_vim}"
+		cmd //c "mklink %USERPROFILE%${link_vimrc} %USERPROFILE%${target_vimrc}" ;;
 esac
